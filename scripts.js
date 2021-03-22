5c8bd72536416a82fb0ed435f5ad2ec94f0a203e
Ermanno <o2H2eZf2@yandex.com>
2021-03-22 14:51:20 +0000

    jhkhhjkjhkjh

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
+
+(function () { if ( 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0 ) {
+  document.documentElement.classList.add("mobile");
+  document.querySelectorAll(".list_item_container").forEach(function (el) { el.classList.add("touch"); });
+};})();
+
+
 // modules are defined as an array
 // [ module function, map of requires ]
 //
